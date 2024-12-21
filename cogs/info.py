01ec35c4f9ecbc7c172bfaa0cc883097aa10d660
jate-koh <jate_ben@hotmail.com>
2024-12-21 20:15:43 +0700

    feat: initial commit

---
+++ cogs/info.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from settings import configs
+
+class Info(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['server'])
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="Server Information",
+            description=f"Server Name: {ctx.guild.name}\nMember Count: {ctx.guild.member_count}\nCreated At: {ctx.guild.created_at}",
+            color=discord.Color.blue()
+        )
+        await ctx.send(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Info(bot))
---
+++ main.py
@@ -0,0 +1,43 @@
+# System Libs
+import sys
+import asyncio
+import os
+
+# Discord Bot Libs
+import discord
+from discord import Intents, app_commands
+from discord.ext import commands
+import dotenv
+from rich import print
+
+# Bot Settings
+from settings import configs
+from cogs import info
+
+print(f"[b yellow] Python version: {sys.version}")
+print(f"[b green] Initializing...")
+intents = Intents.default()
+intents.members = True
+intents.message_content = True
+
+print(f"[b green] Starting bot...")
+
+bot = commands.Bot(command_prefix=configs["prefix"], intents=intents)
+
+@bot.event
+async def on_ready():
+    
+    print(f"[b green] Bot is ready! Logged in as {bot.user}")
+    print(f"[b green] Watching over guilds: {', '.join([guild.name for guild in bot.guilds])}")
+
+    
+print(f"[b green] Loading cogs...")
+
+basics = [
+    info.setup(bot)
+]
+
+for cog in basics:
+    asyncio.run(cog)
+
+bot.run(configs["token"])
